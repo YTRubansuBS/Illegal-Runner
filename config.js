7928efced49eed237666efab1e0d018bd074a718
YTRubansuBS <rubendeclaye@gmail.com>
2026-09-15 13:21:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    Ne mets JAMAIS la clé service_role dans ce fichier.
 */
 window.IR_CONFIG = {
-  SUPABASE_URL: 'YOUR_SUPABASE_URL',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY',
+  SUPABASE_URL: 'https://cgodpoxubzrggyezzvth.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnb2Rwb3h1YnpyZ2d5ZXp6dnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODgzNzUsImV4cCI6MjEwNDk2NDM3NX0.fkA5ih8xU3rIJe7u8p4c0dfiJmzmCmTzUo9md5L4wcU',
   ADMIN_USERNAME: 'Rubansu1'
 };
